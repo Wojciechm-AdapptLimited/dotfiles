58e084a5ef0543fedc12bf5eefdd592ed36f5bff
Wojciechm-AdapptLimited <wojciech.maczka@protonmail.com>
2024-03-21 23:07:53 +0100

    fix(zsh): fix history bindings for MacOS

--- zsh/config/__init__.zsh
+++ zsh/config/__init__.zsh
@@ -92,6 +92,12 @@ function zshrc-post {
   bindkey '^[OA' history-substring-search-up
   bindkey '^[OB' history-substring-search-down
 
+  bindkey '^[[A' history-substring-search-up
+  bindkey '^[[B' history-substring-search-down
+
+  bindkey "$terminfo[kcuu1]" history-substring-search-up
+  bindkey "$terminfo[kcud1]" history-substring-search-down
+
   HISTORY_SUBSTRING_SEARCH_ENSURE_UNIQUE=1
 
   # Init prompt.
