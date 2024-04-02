6480ef2fa1442cf5a3d9954e5ff15b7a79aaad73
Wojciechm-AdapptLimited <wojciech.maczka@protonmail.com>
2024-04-02 14:29:09 +0200

    feat(zsh): add plugin and completions for pyenv

---
+++ zsh/plugins/pyenv/pyenv.plugin.zsh
@@ -0,0 +1,8 @@
+# pyenv
+
+export PYENV_ROOT="$XDG_DATA_HOME/pyenv"
+export PATH="$PATH:$XDG_DATA_HOME/pyenv"
+
+[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+
+eval "$(pyenv init --path)"
