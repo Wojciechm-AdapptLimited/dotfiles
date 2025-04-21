153612be80dbe702e2f2037b9d6588ac7b832f72
Wojciechm-AdapptLimited <wojciech.maczka@protonmail.com>
2025-04-21 15:52:55 +0200

    feat(zsh): use bat as a manpager

--- zsh/plugins/bat/bat.plugin.zsh
+++ zsh/plugins/bat/bat.plugin.zsh
@@ -4,8 +4,10 @@ if (( ! $+commands[bat] )); then
 fi
 
 alias cat='bat'
+alias man='batman'
+export MANPAGER=env\ BATMAN_IS_BEING_MANPAGER=yes\ zsh\ /usr/bin/batman
+export MANROFFOPT=-c
 
-export MANPAGER="sh -c 'col -bx | bat -l man -p'"
 
 # alias -g -- -h='-h 2>&1 | bat --language=help --style=plain'
 alias -g -- --help='--help 2>&1 | bat --language=help --style=plain'
