6ed4fe167a1a25b3ca12a5b5dc349639a96140b8
Wojciechm-AdapptLimited <wojciech.maczka@protonmail.com>
2025-07-29 14:36:38 +0200

    feat(nvim): rewrite copilot setup to use blink

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -16,6 +16,7 @@ return {
 			end)(),
 			dependencies = {
 				"rafamadriz/friendly-snippets",
+				"giuxtaposition/blink-cmp-copilot",
 				config = function()
 					require("luasnip.loaders.from_vscode").lazy_load()
 				end,
@@ -78,10 +79,64 @@ return {
 		},
 
 		sources = {
-			default = { "lsp", "buffer", "snippets", "path" },
+			default = { "lsp", "buffer", "snippets", "path", "copilot" },
+			providers = {
+				copilot = {
+					name = "copilot",
+					module = "blink-cmp-copilot",
+					score_offset = 100,
+					async = true,
+					transform_items = function(_, items)
+						local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
+						local kind_idx = #CompletionItemKind + 1
+						CompletionItemKind[kind_idx] = "Copilot"
+						for _, item in ipairs(items) do
+							item.kind = kind_idx
+						end
+						return items
+					end,
+				},
+			},
 		},
 
 		signature = { enabled = true },
+
+		appearance = {
+			-- Blink does not expose its default kind icons so you must copy them all (or set your custom ones) and add Copilot
+			kind_icons = {
+				Copilot = "",
+				Text = "󰉿",
+				Method = "󰊕",
+				Function = "󰊕",
+				Constructor = "󰒓",
+
+				Field = "󰜢",
+				Variable = "󰆦",
+				Property = "󰖷",
+
+				Class = "󱡠",
+				Interface = "󱡠",
+				Struct = "󱡠",
+				Module = "󰅩",
+
+				Unit = "󰪚",
+				Value = "󰦨",
+				Enum = "󰦨",
+				EnumMember = "󰦨",
+
+				Keyword = "󰻾",
+				Constant = "󰏿",
+
+				Snippet = "󱄽",
+				Color = "󰏘",
+				File = "󰈔",
+				Reference = "󰬲",
+				Folder = "󰉋",
+				Event = "󱐋",
+				Operator = "󰪚",
+				TypeParameter = "󰬛",
+			},
+		},
 	},
 	opts_extend = { "sources.default" },
 }
--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,37 +1,6 @@
 return {
 	"zbirenbaum/copilot.lua",
 	build = ":Copilot auth",
-
-	dependencies = {
-		{
-			"zbirenbaum/copilot-cmp",
-			event = { "InsertEnter", "LspAttach" },
-			config = function(_, opts)
-				local function on_att(on_attach)
-					vim.api.nvim_create_autocmd("LspAttach", {
-						callback = function(args)
-							local buffer = args.buf
-							local client = vim.lsp.get_client_by_id(args.data.client_id)
-							on_attach(client, buffer)
-						end,
-					})
-				end
-
-				local copilot_cmp = require("copilot_cmp")
-				copilot_cmp.setup(opts)
-
-				-- attach cmp source whenever copilot attaches
-				-- fixes lazy-loading issues with the copilot cmp source
-				-- https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/plugins/extras/coding/copilot.lua#L61
-				on_att(function(client)
-					if client.name == "copilot" then
-						copilot_cmp._on_insert_enter({})
-					end
-				end)
-			end,
-		},
-	},
-
 	config = function()
 		require("copilot").setup({
 			suggestion = { enabled = false },
@@ -41,6 +10,6 @@ return {
 			},
 		})
 
-		vim.keymap.set("n", "<leader>cc", "<cmd>Copilot attach<CR>", { desc = "[C]opilot" })
+		vim.keymap.set("n", "<leader>ag", "<cmd>Copilot attach<CR>", { desc = "[G]ithub Copilot" })
 	end,
 }
