b6ae44fd0c9f0f1ace2b0fcac3d3abd0eaf09ae8
Wojciechm-AdapptLimited <wojciech.maczka@protonmail.com>
2025-10-24 17:24:08 +0200

    fix(nvim): double the conform timeout

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -5,7 +5,7 @@ return {
 	opts = {
 		notify_on_error = false,
 		format_on_save = {
-			timeout_ms = 1500,
+			timeout_ms = 3000,
 			lsp_fallback = true,
 		},
 		formatters_by_ft = vim.g.config.tools.formatters,
